f27e2eb043e8ef4daa2bb34414950b4f3f721e9a
PabloSumba <psumbag@est.ups.edu.ec>
2023-04-26 09:28:57 -0500

    variables

---
+++ src/main/java/es/edu/ups/variables/Variables.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.edu.ups.variables;
+
+/**
+ *
+ * @author 59398
+ */
+public class Variables {
+
+    public static void main(String[] args) {
+        
+        int num1, num2, resultado;
+        num1 =4;
+        num2 =2;
+        
+        resultado= num1 + num2;
+        System.out.println("el resultado es "+ resultado);
+       
+      
+                
+    }
+}
